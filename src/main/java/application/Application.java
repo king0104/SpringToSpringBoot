4c039a3fb9f52dbb940f9512c9f58dded84c76d8
king0104 <4545abc@naver.com>
2021-09-22 21:00:07 +0900

    inital commit

---
+++ src/main/java/application/Application.java
@@ -0,0 +1,17 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@Configuration
+//@ImportResource(value={"classpath/src/main/webapp/WEB-INF/web.xml"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(Application.class);
+        application.run(args);
+    }
+}
